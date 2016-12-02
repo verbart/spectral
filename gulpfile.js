bf7059834e28760c53cbec18f3ad6e1c23736311
Artem Verbat <artem.verbat@gmail.com>
2016-12-02 15:19:17 +0200

    Update structure. A litle refactoring.

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,95 @@
 const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
 const stylus = require('gulp-stylus');
-const pug = require('gulp-pug');
-const posthtml = require('gulp-posthtml');
-const posthtmlBEM =  require('posthtml-bem');
-const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
 const gulpIf = require('gulp-if');
-const notify = require('gulp-notify');
-const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
 const del = require('del');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cleanCSS = require('gulp-clean-css');
+const gutil = require('gulp-util');
+const pug = require('gulp-pug');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 
-gulp.task('views', function () {
-  return gulp.src('pages/**/*.pug')
-    .pipe(plumber({
-      errorHandler: notify.onError(function (err) {
-        return {
-          message: err.message
-        };
-      })
-    }))
-    .pipe(pug({
-      pretty: isDevelopment
-    }))
-    .pipe(posthtml([
-      posthtmlBEM({
-        elemPrefix: '__',
-        modPrefix: '_',
-        modDlmtr: '_'
-      })
-    ]))
-    .pipe(gulp.dest('./public'));
+gulp.task('views', function buildHTML() {
+    return gulp.src('./src/index.pug')
+        .pipe(pug())
+        .on('error', function(error) {
+            gutil.log(gutil.colors.red('Error: ' + error.message));
+            this.emit('end');
+        })
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('styles', function () {
-  return gulp.src('./pages/**/*.styl')
-    .pipe(plumber({
-      errorHandler: notify.onError(function (err) {
-        return {
-          message: err.message
-        };
-      })
-    }))
-    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
-    .pipe(stylus())
-    .pipe(postcss([
-      autoprefixer({
-        browsers: ['> 5%', 'ff > 14']
-      })
-    ]))
-    .pipe(gulpIf(isDevelopment, sourcemaps.write('./')))
-    .pipe(gulpIf(!isDevelopment, cleanCSS()))
-    .pipe(gulp.dest('./public'));
+    return gulp.src('./src/app.styl')
+        .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+        .pipe(stylus({
+            'include css': true
+        })
+            .on('error', function(error) {
+                gutil.log(gutil.colors.red('Error: ' + error.message));
+                this.emit('end');
+            }))
+        .pipe(gulpIf(!isDevelopment, postcss([
+            autoprefixer({
+                browsers: ['> 5%', 'ff > 14']
+            })
+        ])))
+        .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+        .pipe(gulpIf(!isDevelopment, cleanCSS()))
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('./dist/css'))
 });
 
-gulp.task('images', function () {
-  return gulp.src('./blocks/**/*.{png,jpg,gif,svg}')
-    .pipe(rename(function (path) {
-      path.dirname = '';
-    }))
-    .pipe(gulp.dest('./public/images'));
+gulp.task('copy:fonts', function () {
+    return gulp.src([
+        './node_modules/font-awesome/fonts/**/*.*'
+    ])
+        .pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('copy:images', function () {
+    return gulp.src('./src/**/*.{png,jpg,jpeg,gif,svg}')
+        .pipe(rename(function (path) {
+            path.dirname = '';
+        }))
+        .pipe(gulp.dest('./dist/images'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./{blocks,pages}/**/*.pug', gulp.series('views'));
-  gulp.watch('./{blocks,pages}/**/*.styl', gulp.series('styles'));
+    gulp.watch('./src/**/*.pug', gulp.series('views'));
+    gulp.watch('./src/**/*.styl', gulp.series('styles'));
 });
 
 gulp.task('serve', function () {
-  browserSync.init({
-    server: './public',
-    port: 8080
-  });
+    browserSync.init({
+        server: './dist',
+        port: 8080
+    });
 
-  browserSync.watch('./**/*.html').on('change', browserSync.reload);
-  browserSync.watch('./**/*.css').on('change', browserSync.reload);
+    browserSync.watch('./dist/**/*.*').on('change', browserSync.reload);
 });
 
 gulp.task('clean', function () {
-  return del('./public')
+    return del('./dist')
 });
 
 gulp.task('build', gulp.series(
-  'clean',
-  gulp.parallel(
-    'images',
-    'views',
-    'styles'
-)));
+    'clean',
+    gulp.parallel(
+        'views',
+        'styles',
+        'copy:fonts',
+        'copy:images'
+    )));
 
 gulp.task('default', gulp.series(
-  'build',
-  gulp.parallel(
-    'watch',
-    'serve'
-)));
+    'build',
+    gulp.parallel(
+        'watch',
+        'serve'
+    )));
